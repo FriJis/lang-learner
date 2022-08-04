c9a3ac8c075e89aabc3779f90480d745f2b9f1b9
Egor Kuzmin <egorik580@gmail.com>
2022-08-04 16:35:37 +0300

    hotfix

--- src/conf.ts
+++ src/conf.ts
@@ -68,7 +68,7 @@ export const langs = [
     ['Divehi, Dhivehi, Maldivian', 'dv'],
     ['Dutch', 'nl'],
     ['Dzongkha', 'dz'],
-    ['English', 'en_EN'],
+    ['English', 'en-EN'],
     ['Esperanto', 'eo'],
     ['Estonian', 'et'],
     ['Ewe', 'ee'],
