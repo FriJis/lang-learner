f56a353391ca998e6be23e0121dbe66a737e2db4
Egor Kuzmin <egorik580@gmail.com>
2024-02-02 18:17:52 +0200

    updated quiz & control work

--- src/pages/learn/learn.tsx
+++ src/pages/learn/learn.tsx
@@ -1,18 +1,6 @@
-import {
-    Button,
-    CardActions,
-    CardContent,
-    Checkbox,
-    Dialog,
-    DialogTitle,
-    FormControlLabel,
-    FormGroup,
-    Grid,
-    Typography,
-} from '@mui/material'
+import { Dialog, DialogTitle } from '@mui/material'
 import _ from 'lodash'
 import { useCallback, useEffect, useState } from 'react'
-import { Info } from '../../components/Info'
 import { Nothing } from '../../components/Nothing'
 import { lsConf } from '../../conf'
 import { useLS } from '../../hooks/useLS'
@@ -20,7 +8,7 @@ import { useUpdateProgress } from '../../hooks/useUpdateProgress'
 import { Word } from '../../types/word'
 import { getRandomValueFromArray, sayNative, sayTranslation } from '../../utils'
 import { composeWords } from '../../utils/db'
-import { Card, Cards } from '../../components/Card'
+import { Quiz } from '../../components/Quiz'
 
 export const LearnComponent = () => {
     const [ready, setReady] = useState(false)
@@ -28,8 +16,6 @@ export const LearnComponent = () => {
     const [word, setWord] = useState<Word | null>(null)
     const [prev, setPrev] = useState<Word | null>(null)
     const [showPrev, setShowPrev] = useState(false)
-    const [langChanging, setLangChanging] = useState(false)
-    const [reverse, setReverse] = useState(true)
 
     const [translations, setTranslations] = useState<Word[]>([])
 
@@ -54,33 +40,25 @@ export const LearnComponent = () => {
             countWords - 1
         )
 
-        setReverse((o) => {
-            if (!langChanging) return false
-            if (_.random(0, 100) <= 50) return !o
-            return !o
-        })
         setWord(randomWord)
         setTranslations(_.shuffle([randomWord, ...preparedWords]))
-    }, [countWords, learnFirst, prev, langChanging])
+    }, [countWords, learnFirst, prev])
 
-    const compare = useCallback(
-        async (translation?: Word) => {
-            if (!word) return
+    const success = async () => {
+        if (!word) return
+        await updater?.success()
+        setPrev(word)
+    }
 
-            if (word.id === translation?.id) {
-                await updater?.success()
-                setPrev(word)
-                return
-            }
-            sayNative(word.native)
-            sayTranslation(word.translation)
-            setShowPrev(true)
-            await updater?.fail()
+    const fail = async () => {
+        if (!word) return
+        sayNative(word.native)
+        sayTranslation(word.translation)
+        setShowPrev(true)
+        await updater?.fail()
 
-            setPrev(word)
-        },
-        [word, updater]
-    )
+        setPrev(word)
+    }
 
     useEffect(() => {
         generate().then(() => setReady(true))
@@ -88,78 +66,22 @@ export const LearnComponent = () => {
 
     if (!ready) return null
 
-    if (translations.length <= 0) return <Nothing />
+    if (!word) return <Nothing />
 
     return (
-        <Cards>
+        <>
             <Dialog onClose={() => setShowPrev(false)} open={showPrev}>
                 <DialogTitle>
                     {prev?.native} - {prev?.translation}{' '}
                     {!!prev?.info ? `(${prev.info})` : ''}
                 </DialogTitle>
             </Dialog>
-            <Card>
-                <CardContent>
-                    <Typography
-                        align="center"
-                        onMouseEnter={() =>
-                            reverse
-                                ? sayTranslation(word?.translation || '')
-                                : sayNative(word?.native || '')
-                        }
-                        onMouseLeave={() => window.speechSynthesis.cancel()}
-                    >
-                        {reverse ? word?.translation : word?.native || ''}{' '}
-                        {!!word?.info && <Info word={word}></Info>}
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    <Grid container justifyContent="center">
-                        {translations.map((t) => (
-                            <Grid item key={t.id}>
-                                <Button
-                                    color="success"
-                                    onClick={() => compare(t)}
-                                    onMouseEnter={() =>
-                                        reverse
-                                            ? sayNative(t.native)
-                                            : sayTranslation(t.translation)
-                                    }
-                                    onMouseLeave={() =>
-                                        window.speechSynthesis.cancel()
-                                    }
-                                >
-                                    {reverse ? t.native : t.translation}
-                                </Button>
-                            </Grid>
-                        ))}
-                    </Grid>
-                </CardActions>
-                <CardActions>
-                    <Grid container>
-                        <Grid item>
-                            <Button color="error" onClick={() => compare()}>
-                                I don't know
-                            </Button>
-                        </Grid>
-                    </Grid>
-                </CardActions>
-            </Card>
-            <Card>
-                <CardContent>
-                    <FormGroup>
-                        <FormControlLabel
-                            control={
-                                <Checkbox
-                                    value={langChanging}
-                                    onChange={() => setLangChanging((o) => !o)}
-                                />
-                            }
-                            label="Realtime language changing"
-                        />
-                    </FormGroup>
-                </CardContent>
-            </Card>
-        </Cards>
+            <Quiz
+                currentWord={word}
+                answerOptions={translations}
+                onFail={fail}
+                onSuccess={success}
+            />
+        </>
     )
 }
